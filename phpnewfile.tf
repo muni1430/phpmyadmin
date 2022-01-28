32fbe4ccc0e56422b03110a995b2d7bbd00397e5
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-28 08:38:55 +0530

    Update phpnewfile.tf

--- phpnewfile.tf
+++ phpnewfile.tf
@@ -29,3 +29,16 @@ resource "aws_db_instance" "default" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
+resource "aws_instance" "myec2" {
+  depends_on = [aws_db_instance.default]
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  subnet_id   = "subnet-04bb8948"
+  key_name = "terra"
+  user_data = "templatefile("${path.module}/userdata.tftpl", {endpoint = aws_db_instance.default.endpoint},{username = aws_db_instance.default.username},{password = aws_db_instance.default.password})"
+  iam_instance_profile = "demo_full_access"
+  security_groups = ["sg-0244e3f85210cc582"]
+  tags = {
+    Name = "Ec2tf"
+  }
+}
